9a063df26324fd7cc01d7d933de3f71460657ed9
beydo <ben@redmedia.com>
2025-04-26 16:04:58 +0200

    Add files via upload

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Bienvenue 👋</h1>
+      <p>This is your mobile-friendly French Verbs Trainer PWA.</p>
+    </div>
+  );
+}
